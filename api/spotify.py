dec4ce14153c33d6b0b2443867200666ea44275c
alexac54767 <alexarosecarlson@icloud.com>
2023-05-18 00:02:01 -0700

    adding get album and get artist

--- api/spotify.py
+++ api/spotify.py
@@ -3,3 +3,18 @@
 
 # Spotify Info <- use this it's literally amazing: https://developer.spotify.com/documentation/web-api
 
+# GET Album
+wget --quiet \
+  --method GET \
+  --header 'Authorization: Bearer BQDT1E...x4Phso' \
+  --output-document \
+  - https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy
+
+
+# GET Artist
+wget --quiet \
+  --method GET \
+  --header 'Authorization: Bearer BQALbP...DSPCOf' \
+  --output-document \
+  - https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg
+
