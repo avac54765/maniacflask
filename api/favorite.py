366065fe15ba18881c574d86ce2e5ada90e5e306
alexac54767 <alexarosecarlson@icloud.com>
2023-05-18 12:35:33 -0700

    new favortie database

---
+++ api/favorite.py
@@ -0,0 +1,68 @@
+from flask import Blueprint, request, jsonify
+from flask_restful import Api, Resource 
+from datetime import datetime
+
+from model.ISPEs import ISPE
+
+# CODE INSPO CREDIT: John Mortensen
+
+ISPE_api = Blueprint('ISPE_api', __name__,
+                   url_prefix='/api/ISPE')
+
+
+api = Api(ISPE_api)
+
+# create the API for ISPE
+class ISPEAPI:        
+    class _Create(Resource):
+        def post(self):
+            ''' Read data for json body '''
+            body = request.get_json()
+            
+            # setting parameters for garbage data
+            ''' Avoid garbage in, error checking '''
+            
+            name2 = body.get('name2')
+            if name2 is None or len(name2) < 2:
+                return {'message': f'Name is missing, or is less than 2 characters'}, 211
+            
+            id = body.get('id')
+            duration2 = body.get('duration2')
+            if duration2 is None or not int:
+                return {'message': f'Duration is missing, or is not an integer'}, 212
+            date2 = body.get('date2')
+            grade = body.get('grade')
+            uid = str(datetime.now()) # temporary UID that is unique to fill garbage data
+            if uid is None or len(uid) < 2:
+                return {'message': f'User ID is missing, or is less than 2 characters'}, 214
+
+            from model.ISPEs import ISPE
+            io = ISPE(id=id,
+                      uid=uid,
+                      name2=name2,
+                      duration2=duration2,
+                      date2=date2,
+                      grade=grade,
+                    )
+            
+
+            
+            ''' #2: Key Code block to add user to database '''
+           
+            ISPE = io.create()
+
+            
+            if ISPE:
+                return jsonify(ISPE.read())
+            # failure returns error
+            return {'message': f'Processed {name2}, either a format error or User ID {uid} is duplicate'}, 215
+
+    class _Read(Resource):
+        def get(self):
+            ISPEs = ISPE.query.all()    # read database
+            json_ready = [ISPE.read() for ISPE in ISPEs]  # json output
+            return jsonify(json_ready)  
+
+    # RESTapi endpoints
+    api.add_resource(_Create, '/create')
+    api.add_resource(_Read, '/')
---
+++ model/favorites.py
@@ -0,0 +1,153 @@
+""" database dependencies to support sqliteDB examples """
+from random import randrange
+from datetime import date
+import os, base64
+import json
+
+from __init__ import app, db
+from sqlalchemy.exc import IntegrityError
+
+
+# Define ISPE class
+class FAV(db.Model):
+    __tablename__ = 'FAVS'
+
+    #  Notes schema
+    id = db.Column(db.Integer, primary_key=True)
+    _uid = db.Column(db.String(255), unique=True, nullable=False)
+    _songname = db.Column(db.String, unique=True, nullable=False)
+    _artist = db.Column(db.Integer, unique=False, nullable=False)
+    _album = db.Column(db.Integer, unique=False, nullable=False)
+
+
+    
+    # Notes object
+    def __init__(self, id, uid, songname, artist, album):
+        self.userID = id
+        self._uid = uid
+        self._songname = songname
+        self._artist = artist
+        self._album = album
+        
+    
+    
+    # getter method
+    @property
+    def uid(self):
+        return self._uid
+    
+    # setter function
+    @uid.setter
+    def uid(self, uid):
+        self._uid = uid
+        
+   
+    def is_uid(self, uid):
+        return self._uid == uid
+
+
+     # getter method
+    @property
+    def songname(self):
+        return self._songname
+    
+    # setter function
+    @songname.setter
+    def songname(self, songname):
+        self._songname = songname
+
+     # getter method
+    @property
+    def artist(self):
+        return self._artist
+    
+    # setter function
+    @artist.setter
+    def artist(self, artist):
+        self._artist = artist
+    
+     # getter method
+    @property
+    def album(self):
+        return self._album
+    
+    # setter function
+    @album.setter
+    def album(self, album):
+        self._album = album
+
+    
+    
+    
+    
+
+    # json dumps output
+    def __str__(self):
+        return json.dumps(self.read())
+
+
+    # CRUD create to add new record
+    def create(self):
+        try:
+            db.session.add(self)  
+            db.session.commit()  
+            return self
+        except IntegrityError:
+            db.session.remove()
+            return None
+
+    # CRUD read returns dictionary
+    def read(self):        
+        return {
+            "id": self.id,
+            "uid": self.uid,
+            "songname": self.songname,
+            "artist": self.artist,
+            "album": self.album
+        }
+
+   #  def update(self, uid='', duration2=''):
+    #    """only updates values with length"""
+    #    if len(uid) > 0:
+     #       self.uid = uid
+      #  if len(duration2) > 0:
+      #      self.duration2 = duration2
+      #  db.session.commit()
+      #  return self
+
+    # CRUD delete: remove item
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return None
+
+
+"""Database Creation and Testing """
+
+
+# making tester data to test database creation
+def initISPEs():
+    with app.app_context():
+        """Create database and tables"""
+        db.create_all()
+        """Tester data for table"""
+        i1 = FAV(id='33', uid='alexa', songname='Home Movies', artist='Beach Weather', album='Chit Chat')
+        i2 = FAV(id='24', uid='ava', songname='Night Changes', artist='One Direction', album='Four')
+        i3 = FAV(id='56', uid='TomHolland', songname='A Thousand Years', artist='James Arthur', album='single')
+        i4 = FAV(id='23', uid='dylan', songname='past life', artist='Elijah Woods', album='single')
+        i5 = FAV(id='59', uid='jm1021', songname='on life', artist='Sammy Rash', album='single')
+
+        favs = [i1, i2, i3, i4, i5]
+
+        """Builds sample user/note(s) data"""
+        for fav in favs:
+            try:
+                '''add a few 1 to 4 notes per user'''
+                for num in range(randrange(1, 4)):
+                    '''add FAV data to table'''
+                    fav.create()
+            except IntegrityError:
+                '''fails with bad or duplicate data'''
+                db.session.remove()
+                print(f"Records exist, duplicate email, or error: {FAV.id}")
+    
